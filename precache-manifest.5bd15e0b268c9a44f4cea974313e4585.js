a0810bdde1824d108e2ffd3c2ca8896736a9d355
nukeair <nukeair@sansa-2.local>
2020-12-04 16:33:26 +0800

    Updates

---
+++ precache-manifest.5bd15e0b268c9a44f4cea974313e4585.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06f8b39c73ba3db2b0874fbc87f43962",
+    "url": "/flexi-45/index.html"
+  },
+  {
+    "revision": "2285061ad071e6384331",
+    "url": "/flexi-45/static/css/2.6414bb7b.chunk.css"
+  },
+  {
+    "revision": "cad7c34ea2be09112c96",
+    "url": "/flexi-45/static/css/main.474702da.chunk.css"
+  },
+  {
+    "revision": "2285061ad071e6384331",
+    "url": "/flexi-45/static/js/2.af840ca0.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/flexi-45/static/js/2.af840ca0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cad7c34ea2be09112c96",
+    "url": "/flexi-45/static/js/main.15fab09c.chunk.js"
+  },
+  {
+    "revision": "5d5974e20e3d99e4db03",
+    "url": "/flexi-45/static/js/runtime-main.8368c3e1.js"
+  }
+]);
